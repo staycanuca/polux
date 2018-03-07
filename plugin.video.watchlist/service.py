13686df61bae8356bd2db1145e038d1c5f9b2d78
staycanuca <staycanuca@gmail.com>
2018-03-07 22:24:32 +0200

    watchlist

---
+++ plugin.video.watchlist/service.py
@@ -0,0 +1,21 @@
+﻿# -*- coding: utf-8 -*-
+import xbmc
+
+from resources.lib.misc import *
+from resources.lib.db import DB
+
+
+class Monitor(xbmc.Monitor):
+    pass
+
+
+if __name__ == '__main__':
+    db = DB(os.path.join(addonprofile, 'database.db'))
+    monitor = Monitor()
+    try:
+        monitor.waitForAbort()
+    except AttributeError:
+        # Below for Gotham support
+        while not xbmc.abortRequested:
+            xbmc.sleep(500)
+    xbmc.log("Watchlist Service done!", level=xbmc.LOGNOTICE)
